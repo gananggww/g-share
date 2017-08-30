a3bbb752e1134de093a8c52e11b067e233a14cb2
anggraito <anggraito.laras@gmail.com>
2017-08-30 20:41:32 +0700

    add app.js with mongoo atlas

--- server/app.js
+++ server/app.js
@@ -1,19 +1,18 @@
 const express = require("express")
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors')
 const index = require('./router/index');
 const userRouter = require('./router/userRouter')
 const app = express()
 
 app.use(cors())
-mongoose.connect('mongodb://localhost/g-share', (err)=>{
-  if(err){
-    console.log(err);
-  }else{
-    console.log("database conected");
-  }
+var url = "mongodb://anggraito:<PASSWORD>@mydb-shard-00-00-vv98n.mongodb.net:27017,mydb-shard-00-01-vv98n.mongodb.net:27017,mydb-shard-00-02-vv98n.mongodb.net:27017/test?ssl=true&replicaSet=mydb-shard-0&authSource=admin"
+mongoose.connect(url, (err, db)=>{
+  db.close();
 });
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
